db7026ed41f13592725c4f09e8c8a301dad19de7
Arthur Larangeiras <arthurlarangeiras@gmail.com>
2023-10-27 11:16:24 -0300

    desafio concluido

--- index.js
+++ index.js
@@ -1,5 +1,37 @@
-let name = "Arthur", experience = 8000;
+let nome = "Arthur";
+let experience = 8000;
+let nivel = "";
 
 if (experience < 1000) {
-    console.log("Ferro");
+    nivel = "Ferro";
 }
+
+else if (experience >= 1001 && experience <= 2000) {
+    nivel = "Bronze";
+}
+
+else if (experience >= 2001 && experience <= 5000) {
+    nivel = "Prata";
+}
+
+else if (experience >= 6001 && experience <= 7000) {
+    nivel = "Ouro";
+}
+
+else if (experience >= 7001 && experience <= 8000) {
+    nivel = "Platina";
+}
+
+else if (experience >= 8001 && experience <= 9000) {
+    nivel = "Ascendente";
+}
+
+else if (experience >= 9001 && experience <= 10000) {
+    nivel = "Imortal";
+}
+
+else {
+    nivel = "Radiante";
+}
+
+console.log(`O Herói de nome ${nome} está no nível de ${nivel}`);
